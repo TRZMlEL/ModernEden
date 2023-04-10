cca7694103a8967bcb493e214fb279fa751c5674
TRZMlEL <125200805+TRZMlEL@users.noreply.github.com>
2023-04-10 12:51:18 +0200

    Add files via upload

---
+++ sketch_apr07a.ino
@@ -0,0 +1,74 @@
+#define BUTTON_PIN 4
+void setup()
+{
+  Serial.begin(57600);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+ 
+  int wilgotnoscPW = 0;
+  int temperaturaPW = 0;
+  int swiatlo = 0;
+  int stan_okna = 0;
+  int ProcentowaWilgotnoscGlebyS1 = 0;
+  int ProcentowaWilgotnoscGlebyS2 = 0;
+  int StanDeszczu = 0;
+  int CisnieniePZ = 0;
+  int temperaturaPZ = 0;
+  int Wysokosc = 0;
+  int RzeczywistaWysokosc = 0;
+}
+void loop()
+{
+  byte buttonState = digitalRead(BUTTON_PIN);
+  
+  if (buttonState == LOW) {
+    int wilgotnoscPW = 0;
+    int temperaturaPW = 0;
+    int swiatlo = 0;
+    int stan_okna = 0;
+    int ProcentowaWilgotnoscGlebyS1 = 0;
+    int ProcentowaWilgotnoscGlebyS2 = 0;
+    int StanDeszczu = 0;
+    int CisnieniePZ = 0;
+    int temperaturaPZ = 0;
+    int Wysokosc = 0;
+    int RzeczywistaWysokosc = 0;
+      
+    Serial.println(wilgotnoscPW);                // wilgotność powietrza wewnętrzna 0-100
+    Serial.println(temperaturaPW);               // temperatura powietrza wewnętrzna
+    Serial.println(swiatlo);                     // foterezystor stopień naświtlenia
+    Serial.println(stan_okna);                   // stan onka zamknięte 0 otwarte 1
+    Serial.println(ProcentowaWilgotnoscGlebyS1); // wilgotność gleby strefy 1 0-100
+    Serial.println(ProcentowaWilgotnoscGlebyS2); // wilgotność gleby strefy 2 0-100
+    Serial.println(StanDeszczu);                 // stan czujnika 2 nie pada 1 zagrożenie deszczem 0 deszcz
+    Serial.println(CisnieniePZ);                 // Ciśnienie zewnętrzne        (Pascal)
+    Serial.println(temperaturaPZ);               // temperatura zewnętrzna
+    Serial.println(Wysokosc);                    // wysokość nwm jaka           (metr)
+    Serial.println(RzeczywistaWysokosc);         // wysokość nad poziomem morza (metr)
+  }
+  else {
+    int wilgotnoscPW = 30;
+    int temperaturaPW = 20;
+    int swiatlo = 1;
+    int stan_okna = 1;
+    int ProcentowaWilgotnoscGlebyS1 = 33;
+    int ProcentowaWilgotnoscGlebyS2 = 25;
+    int StanDeszczu = 1;
+    int CisnieniePZ = 20;
+    int temperaturaPZ = 10;
+    int Wysokosc = 2;
+    int RzeczywistaWysokosc = 175;
+    
+    Serial.println(wilgotnoscPW);                // wilgotność powietrza wewnętrzna 0-100
+    Serial.println(temperaturaPW);               // temperatura powietrza wewnętrzna
+    Serial.println(swiatlo);                     // foterezystor stopień naświtlenia
+    Serial.println(stan_okna);                   // stan onka zamknięte 0 otwarte 1
+    Serial.println(ProcentowaWilgotnoscGlebyS1); // wilgotność gleby strefy 1 0-100
+    Serial.println(ProcentowaWilgotnoscGlebyS2); // wilgotność gleby strefy 2 0-100
+    Serial.println(StanDeszczu);                 // stan czujnika 2 nie pada 1 zagrożenie deszczem 0 deszcz
+    Serial.println(CisnieniePZ);                 // Ciśnienie zewnętrzne        (Pascal)
+    Serial.println(temperaturaPZ);               // temperatura zewnętrzna
+    Serial.println(Wysokosc);                    // wysokość nwm jaka           (metr)
+    Serial.println(RzeczywistaWysokosc);         // wysokość nad poziomem morza (metr)
+  }
+  delay(500);
+}
